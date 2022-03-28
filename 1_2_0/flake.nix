ac9650390443a466a523acb99e2832b6a7d7b310
hugosenari <hugosenari@gmail.com>
2022-03-27 22:04:04 -0300

    feat(nimpkgs): remove lock

--- 1_2_0/flake.nix
+++ 1_2_0/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-optional_t-1_2_0.ref   = "refs/tags/1.2.0";
   inputs.src-optional_t-1_2_0.owner = "flaviut";
   inputs.src-optional_t-1_2_0.repo  = "optional_t";
-  inputs.src-optional_t-1_2_0.dir   = "";
   inputs.src-optional_t-1_2_0.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
